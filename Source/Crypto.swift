c18fc5cea3754e06c75545c09588b9eafbaebf37
Oleksandr Deundiak <Deundiak@gmail.com>
2017-09-28 18:43:58 +0300

    Updated to Xcode9 & swift4

--- Source/Crypto.swift
+++ Source/Crypto.swift
@@ -17,7 +17,7 @@ import Foundation
     ///   - privateKey: the private key of the identity whose signature is going to be generated
     /// - Returns: signature data
     /// - Throws: correspoding error
-    func generateSignature(of data: Data, using privateKey: PrivateKey) throws -> Data
+    @objc func generateSignature(of data: Data, using privateKey: PrivateKey) throws -> Data
     
     /// Verifies the passed-in signature.
     ///
@@ -26,25 +26,25 @@ import Foundation
     ///   - data: the data to be verified
     ///   - publicKey: the public key of the identity whose signature is going to be verified
     /// - Throws: error if verification failed
-    func verifySignature(_ signature: Data, of data: Data, with publicKey: PublicKey) throws
+    @objc  func verifySignature(_ signature: Data, of data: Data, with publicKey: PublicKey) throws
     
     /// Computes SHA-256.
     ///
     /// - Parameter data: the data to be hashed
     /// - Returns: the resulting hash value
-    func computeSHA256(for data: Data) -> Data
+    @objc func computeSHA256(for data: Data) -> Data
     
     /// Imports public key from its raw data representation.
     ///
     /// - Parameter data: raw public key representation
     /// - Returns: imported public key
     /// - Throws: corresponding error
-    func importPublicKey(from data: Data) throws -> PublicKey
+    @objc func importPublicKey(from data: Data) throws -> PublicKey
     
     /// Exports public key to its raw data representation.
     ///
     /// - Parameter publicKey: public key to be exported
     /// - Returns: raw public key representation
     /// - Throws: corresponding error
-    func exportPublicKey(_ publicKey: PublicKey) throws -> Data
+    @objc func exportPublicKey(_ publicKey: PublicKey) throws -> Data
 }
