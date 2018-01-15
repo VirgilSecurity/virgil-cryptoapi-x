07a040327a7302c82d425e11aae7dfa046ea58a2
Eugen Pivovarov <epivovarov@virgilsecurity.com>
2018-01-15 17:46:38 +0200

    Added PrivateKeyExporter

---
+++ Source/PrivateKeyExporter.swift
@@ -0,0 +1,28 @@
+//
+//  PrivateKeyExporter.swift
+//  VirgilCryptoAPI iOS
+//
+//  Created by Eugen Pivovarov on 1/15/18.
+//  Copyright © 2018 VirgilSecurity. All rights reserved.
+//
+
+import Foundation
+
+/// This protocol is responsible for private keys import/export
+@objc(VSAPrivateKeyExporter) public protocol PrivateKeyExporter {
+    /// Exports data of specified Private key.
+    ///
+    /// - Parameters:
+    ///   - privateKey: the private key to be exported
+    /// - Returns: exported private key data
+    /// - Throws: correspoding error
+    @objc func exportPrivateKey(privateKey: PrivateKey) throws -> Data
+    
+    /// Imports Private Key from data
+    ///
+    /// - Parameters:
+    ///   - data: the data to be imported
+    /// - Returns: imported Private Key instance
+    /// - Throws: error if verification failed
+    @objc func importPrivateKey(data: Data) throws -> PrivateKey
+}
