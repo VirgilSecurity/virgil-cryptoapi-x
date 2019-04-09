5255a86c087378b5b1803a2068fbc79287b4be9d
Eugen Pivovarov <epivovarov@virgilsecurity.com>
2019-04-09 14:30:52 +0300

    Fix copyright in publish-docs.sh

--- CI/publish-docs.sh
+++ CI/publish-docs.sh
@@ -2,10 +2,8 @@
 #
 # Copyright (C) 2015-2019 Virgil Security Inc.
 #
-# Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
-#
 # All rights reserved.
-#PUBLISH_DOCS
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
@@ -33,6 +31,8 @@
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
+#
+# Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
 #
 
 # Settings
